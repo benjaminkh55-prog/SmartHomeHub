13f4bd60cbfee71af9bf5ca55a4cc4b1df919f38
Benjamin Khalili <benjaminkhalili55@gmail.com>
2026-03-23 11:50:21 +0100

    good

---
+++ SmartHomeHub/DEVICES.cs
@@ -0,0 +1,44 @@
+﻿public interface IDevice
+{
+    string Name { get; }
+}
+
+public class Lamp : IDevice
+{
+    public string Name => "Lamp";
+
+    public void TurnOn()
+    {
+        DeviceEventManager.Notify($"{Name} turned ON");
+    }
+
+    public void TurnOff()
+    {
+        DeviceEventManager.Notify($"{Name} turned OFF");
+    }
+}
+
+public class Thermostat : IDevice
+{
+    public string Name => "Thermostat";
+
+    public void SetTemperature(int temp)
+    {
+        DeviceEventManager.Notify($"{Name} set to {temp}°C");
+    }
+}
+
+public class DoorLock : IDevice
+{
+    public string Name => "DoorLock";
+
+    public void Lock()
+    {
+        DeviceEventManager.Notify($"{Name} LOCKED");
+    }
+
+    public void Unlock()
+    {
+        DeviceEventManager.Notify($"{Name} UNLOCKED");
+    }
+}
---
+++ SmartHomeHub/FACADE.cs
@@ -0,0 +1,50 @@
+﻿public class SmartHomeFacade
+{
+    private List<IDevice> devices = new();
+    private CommandInvoker invoker = new();
+    private IModeStrategy mode = new NormalMode();
+
+    public void AddDevice(IDevice device)
+    {
+        devices.Add(device);
+        Logger.Instance.Log($"{device.Name} added");
+    }
+
+    public void SetMode(IModeStrategy newMode)
+    {
+        mode = newMode;
+        Logger.Instance.Log($"Mode changed to {newMode.GetType().Name}");
+    }
+
+    public void RunCommand(ICommand cmd, string action)
+    {
+        if (mode.CanExecute(action))
+        {
+            invoker.AddCommand(cmd);
+        }
+        else
+        {
+            Console.WriteLine("Action blocked by current mode");
+        }
+    }
+
+    public void ExecuteAll()
+    {
+        invoker.Run();
+    }
+
+    public void ReplayLast(int count)
+    {
+        invoker.ReplayLast(count);
+    }
+
+    public void MorningRoutine(Lamp lamp, Thermostat thermostat)
+    {
+        Console.WriteLine("Morning routine...");
+
+        RunCommand(new TurnOnLampCommand(lamp), "ON");
+        RunCommand(new SetTemperatureCommand(thermostat, 22), "TEMP");
+
+        ExecuteAll();
+    }
+}
